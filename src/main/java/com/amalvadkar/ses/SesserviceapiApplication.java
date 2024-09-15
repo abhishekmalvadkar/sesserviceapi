f8ddec7366af05ac16779da4e74833daaf7f5980
Abhishek Malvadkar <abhi@dev.com>
2024-09-15 20:07:58 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/ses/SesserviceapiApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.ses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesserviceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesserviceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/ses/SesserviceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.ses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesserviceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
